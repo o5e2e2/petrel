cd02b45a1d18a859880a17954c601d3f920d95fb
alekspeshkov <aleks.peshkov@gmail.com>
2014-08-02 23:47:44 +0400

    refactoring

--- src/Pool.hpp
+++ src/Pool.hpp
@@ -9,17 +9,19 @@
  **/
 template <class Element>
 class Pool {
-public:
-    typedef std::forward_list<Element> list_t;
-    typedef typename list_t::iterator element_type;
-
-private:
     std::mutex pool_mutex;
 
+    typedef std::forward_list<Element> list_t;
     list_t used;
     list_t ready;
 
 public:
+    typedef typename list_t::iterator element_type;
+
+    Element& operator[] (const element_type& that) { return *std::next(that); }
+    bool empty(const element_type& that) { return that == used.end(); }
+    void clear(element_type& that) { that = used.end(); }
+
     element_type acquire() {
         std::lock_guard<decltype(pool_mutex)> lock{pool_mutex};
 
@@ -42,10 +44,6 @@ public:
             clear(element);
         }
     }
-
-    Element& operator[] (const element_type& that) { return *std::next(that); }
-    bool empty(const element_type& that) { return that == used.end(); }
-    void clear(element_type& that) { that = used.end(); }
 
 };
 
