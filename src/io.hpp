61f690ba3bb9ca0163a068c91c86b5960ee0a4b2
Alexey Peshkov <alexey.peshkov@dalee.ru>
2014-09-29 18:01:29 +0400

    Revert "changed 'char' to 'std::istream::char_type' in io.hpp"
    This reverts commit a9d7e62e485d9d489cf079402e1b05985ead7644.

--- src/io.hpp
+++ src/io.hpp
@@ -6,8 +6,8 @@
 void fail_here(std::istream&);
 void fail_char(std::istream&);
 void fail_pos(std::istream&, std::streampos);
-void fail_rewind(std::istream&);
+void fail_rewind(std::istream& in);
 
-bool operator == (std::istream&, const std::istream::char_type keyword []);
+bool operator == (std::istream&, const char keyword []);
 
 #endif
