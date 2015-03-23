93ed8e98c1edfe8b622c97d4132e2a3178ab842a
Aleks Peshkov <aleks.peshkov@gmail.com>
2015-03-23 14:26:09 +0300

    shallowest of 4 TT replacement shceme

--- src/PerftTT.hpp
+++ src/PerftTT.hpp
@@ -12,22 +12,40 @@ class PerftTT {
         node_count_t perft;
     };
 
-    PerftRecord* record;
+    typedef PerftRecord Bucket[4];
+    Bucket* record;
 
     static Zobrist::_t key(Zobrist z, depth_t d) {
-        return (static_cast<Zobrist::_t>(z) & ~DepthMask) | ((d << 6) & DepthMask);
+        return (static_cast<Zobrist::_t>(z) & ~DepthMask) | ((static_cast<Zobrist::_t>(d) << 6) & DepthMask);
     }
 
 public:
     PerftTT(char* c) : record(reinterpret_cast<decltype(record)>(c)) {}
 
     node_count_t get(Zobrist z, depth_t d) const {
-        return (record->key == key(z, d))? record->perft : 0;
+        for (int i = 0; i < 4; ++i) {
+            if ((*record)[i].key == key(z, d)) { return (*record)[i].perft; }
+        }
+        return 0;
     }
 
     void set(Zobrist z, depth_t d, node_count_t n) {
-        record->key = key(z, d);
-        record->perft = n;
+        Zobrist::_t k = key(z, d);
+        Zobrist::_t smallest_d = ~(static_cast<Zobrist::_t>(0));
+
+        int best = 0;
+        for (int i = 0; i < 4; ++i) {
+            Zobrist::_t rk = (*record)[i].key;
+            if (rk == k) { best = i; break; }
+
+            if (smallest_d > (rk & DepthMask)) {
+                smallest_d = (rk & DepthMask);
+                best = i;
+            }
+        }
+
+        (*record)[best].key = k;
+        (*record)[best].perft = n;
     }
 };
 
