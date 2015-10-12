436c7f759557d630399b33ea3732348eda9e38f6
Aleks Peshkov <aleks.peshkov@gmail.com>
2015-10-12 19:08:16 +0300

    PerftTT 5th-slot final optimization

--- src/PerftTT.cpp
+++ src/PerftTT.cpp
@@ -15,7 +15,7 @@ void PerftTT::popup(::Index<3> i) {
 
     for (::Index<3> j = ::Index<3>::Size-1; j > i; --j) {
         //seek the new slot for i
-        if (!b.b[j].isOk(age) || b.b[j] <= n || b.b[j].getDepth() == 1) {
+        if (!b.b[j].isOk(age) || b.b[j] <= n) {
             for (auto k = i; k < j; ++k) {
                 origin.save(+k, m[k+1]);
             }
@@ -34,13 +34,7 @@ node_count_t PerftTT::get(Zobrist z, depth_t d, SearchInfo& info) {
     FOR_INDEX(::Index<3>, i) {
         if (b.b[i].isKeyMatch(z, d)) {
             ++info[TT_Hit];
-
-            if (!b.b[i].isOk(age)) {
-                //update the age of transpositioned entry
-                ++info[TT_Used];
-            }
             popup(i);
-
             return b.b[i].getNodes();
         }
     }
@@ -63,7 +57,7 @@ node_count_t PerftTT::get(Zobrist z, depth_t d, SearchInfo& info) {
     return 0;
 }
 
-void PerftTT::set(Zobrist z, depth_t d, node_count_t n, SearchInfo& info) {
+void PerftTT::set(Zobrist z, depth_t d, node_count_t n, SearchInfo&) {
     /*
     //replace the same entry
     FOR_INDEX(::Index<3>, i) {
@@ -73,101 +67,35 @@ void PerftTT::set(Zobrist z, depth_t d, node_count_t n, SearchInfo& info) {
             return;
         }
     }
-
-    if (d == 1) {
-        FOR_INDEX(PerftRecordDual::Index, i) {
-            if (i == 1) {
-                b.s.swap();
-                origin.save(3, m[3]);
-            }
-        }
-    }
     */
 
-    Index i = 0;
-    if (b.b[0].isOk(age)) {
-        if (d == 1) {
-            if (b.b[0].getDepth() == 1) {
-                backup(0);
-
-                if (b.b[1].getDepth() == 1) {
-                    origin.save(0, m[1]);
-                    i = 1;
-                    if (b.b[2].getDepth() == 1) {
-                        origin.save(1, m[2]);
-                        i = 2;
-                    }
+    if (d == 1) {
+        /*
+        FOR_INDEX(PerftRecordDual::Index, i) {
+            if (b.s[i].isKeyMatch(z)) {
+                if (i == 1) {
+                    b.s.swap();
+                    origin.save(3, m[3]);
                 }
-            }
-            else {
-                b.s.set(z, n);
-                origin.save(3, m[3]);
                 return;
             }
         }
-        else {
-            if (b.b[1] <= n) {
-                origin.save(0, m[1]);
-                i = 1;
-                if (b.b[2] <= n) {
-                    origin.save(1, m[2]);
-                    i = 2;
-                }
-            }
+        */
+        if (b.b[0].isOk(age) && b.b[0].getDepth() > 1 && !(b.b[0] <= n)) {
+            b.s.set(z, n);
+            origin.save(3, m[3]);
+            return;
         }
     }
-    else {
-        ++info[TT_Used];
 
-        if (b.b[1].isOk(age)) {
-            backup(0);
+    Index i = 0;
+    backup(0);
 
-            if (b.b[1] <= n) {
-                origin.save(0, m[1]);
-                i = 1;
-                if (b.b[2] <= n) {
-                    origin.save(1, m[2]);
-                    i = 2;
-                }
-            }
-        }
-        else if (b.b[2].isOk(age)) {
-            //shallowest of 2
-            if (b.b[0] < b.b[1]) {
-                backup(0);
-                origin.save(0, m[1]);
-            }
-            else {
-                backup(1);
-            }
-
-            i = 1;
-            if (b.b[2] <= n) {
-                origin.save(1, m[2]);
-                i = 2;
-            }
-        }
-        else {
-            //shallowest of 3
-            if (b.b[0] < b.b[1]) {
-                if (b.b[0] < b.b[2]) {
-                    backup(0);
-                    origin.save(0, m[2]);
-                }
-                else {
-                    backup(2);
-                }
-            }
-            else {
-                if (b.b[1] < b.b[2]) {
-                    backup(1);
-                    origin.save(1, m[2]);
-                }
-                else {
-                    backup(2);
-                }
-            }
-
+    if (!b.b[1].isOk(age) || b.b[1] <= n) {
+        origin.save(0, m[1]);
+        i = 1;
+        if (!b.b[2].isOk(age) || b.b[2] <= n) {
+            origin.save(1, m[2]);
             i = 2;
         }
     }
