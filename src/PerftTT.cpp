115e3c4b4d825ac2be2188f6ad4a32b00fdba661
Aleks Peshkov <aleks.peshkov@gmail.com>
2015-11-01 18:34:33 +0300

    PerfTT optimization and simplification

--- src/PerftTT.cpp
+++ src/PerftTT.cpp
@@ -4,10 +4,9 @@
 node_count_t PerftTT::get(Zobrist z, depth_t d) {
     if (d == 1) {
         if (b.s[0].isKeyMatch(z)) {
-            auto n = b.s[0].getNodes();
-
-            return n;
+            return b.s[0].getNodes();
         }
+
         if (b.s[1].isKeyMatch(z)) {
             auto n = b.s[1].getNodes();
 
@@ -20,20 +19,16 @@ node_count_t PerftTT::get(Zobrist z, depth_t d) {
     if (b.b[0].isKeyMatch(z, d)) {
         auto n = b.b[0].getNodes();
 
-        if (!b.b[1].isOk(age) || b.b[1].getDepth() <= d) {
-            Index i = 1;
+        if (b.b[1].getDepth() <= d) {
             b.b[0].updateAge(age);
+            Index i = 1;
             origin.save(0, m[1]);
-            if (!b.b[2].isOk(age) || b.b[2].getDepth() <= d) {
+
+            if (b.b[2].getDepth() <= d) {
                 i = 2;
                 origin.save(1, m[2]);
             }
             origin.save(i, m[0]);
-            return n;
-        }
-        if (!b.b[0].isOk(age)) {
-            b.b[0].updateAge(age);
-            origin.save(0, m[0]);
         }
         return n;
     }
@@ -41,27 +36,16 @@ node_count_t PerftTT::get(Zobrist z, depth_t d) {
     if (b.b[1].isKeyMatch(z, d)) {
         auto n = b.b[1].getNodes();
 
-        if (!b.b[2].isOk(age) || b.b[2].getDepth() <= d) {
+        if (b.b[2].getDepth() <= d) {
             b.b[1].updateAge(age);
             origin.save(1, m[2]);
             origin.save(2, m[1]);
-            return n;
-        }
-        if (!b.b[1].isOk(age)) {
-            b.b[1].updateAge(age);
-            origin.save(1, m[1]);
         }
         return n;
     }
 
     if (b.b[2].isKeyMatch(z, d)) {
-        auto n = b.b[2].getNodes();
-
-        if (!b.b[2].isOk(age)) {
-            b.b[2].updateAge(age);
-            origin.save(2, m[2]);
-        }
-        return n;
+        return b.b[2].getNodes();
     }
 
     return 0;
@@ -69,17 +53,13 @@ node_count_t PerftTT::get(Zobrist z, depth_t d) {
 
 void PerftTT::set(Zobrist z, depth_t d, node_count_t n) {
     if (d == 1 && b.b[0].isOk(age) && b.b[0].getDepth() > 1) {
-        if (!b.s[0].isKeyMatch(z)) {
-            b.s.set(z, n);
-            origin.save(3, m[3]);
-        }
+        b.s.set(z, n);
+        origin.save(3, m[3]);
         return;
     }
 
     if (b.b[0].getDepth() == 1) {
-        /*if (!b.s[0].isKeyMatch(b.b[0].getZobrist()))*/ {
-            b.s.set(b.b[0].getZobrist(), b.b[0].getNodes());
-        }
+        b.s.set(b.b[0].getZobrist(), b.b[0].getNodes());
     }
 
     Index i = 0;
